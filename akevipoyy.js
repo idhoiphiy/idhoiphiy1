ba4c06c840cccec615b33e95bf50afd05c960b0c
idhoiphiy <idhoiphiy.o@gmail.com>
2026-04-28 14:57:57 +0700

    Delete akevipoyy.js

--- akevipoyy.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-
-'https://vipoy.co/view/jt8olwxg',
-'https://vipoy.co/view/jt8olwxg',
-'https://vipoy.co/view/jt8olwxg'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
