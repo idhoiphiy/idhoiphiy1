887cd830db455e6f2aa0b32dd6434f99f31185bf
idhoiphiy <idhoiphiy.o@gmail.com>
2026-04-29 23:17:17 +0700

    Create akeadverrt2.js

---
+++ akeadverrt2.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://data527.click/cfd9acb7208b71915d61/598148440a/?placementName=default',
+'https://data527.click/cfd9acb7208b71915d61/598148440a/?placementName=default',
+'https://data527.click/cfd9acb7208b71915d61/598148440a/?placementName=default'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
