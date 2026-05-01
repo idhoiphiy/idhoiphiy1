51aafa57a136d0f252b46a5c3f582e9c145f2e2e
idhoiphiy <idhoiphiy.o@gmail.com>
2026-05-01 15:07:26 +0700

    Create akeadstermmi1.js

---
+++ akeadstermmi1.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://pantomimemailman.com/n38keba9p0?key=5faea91b4ab00ef9e749baf612ab8448',
+'https://pantomimemailman.com/n38keba9p0?key=5faea91b4ab00ef9e749baf612ab8448',
+'https://pantomimemailman.com/n38keba9p0?key=5faea91b4ab00ef9e749baf612ab8448'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
