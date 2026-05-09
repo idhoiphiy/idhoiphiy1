ede4b8cff71ed0dc541c21ee2273c56bafbbe07d
idhoiphiy <idhoiphiy.o@gmail.com>
2026-05-09 16:35:41 +0700

    Create bloggreddadstmingg1.js

---
+++ bloggreddadstmingg1.js
@@ -0,0 +1,26 @@
+var dataUrl = [
+
+'https://playynowvideoox.blogspot.com/2026/05/play-video-20.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-19.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-18.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-17.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-16.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-15.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-14.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-13.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-12.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-11.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-10.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-9.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-8.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-7.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-6.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-5.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-4.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-3.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-2.html',
+'https://playynowvideoox.blogspot.com/2026/05/play-video-1.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
