415a1ce9d8de554977bc7b9451875ddab66a70de
idhoiphiy <idhoiphiy.o@gmail.com>
2026-05-09 16:37:34 +0700

    Create akeadstminggu1.js

---
+++ akeadstminggu1.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://arkgleamfox.com/wkr4hi9v?key=57ee67e855faf38e4c7feb082dddb780',
+'https://arkgleamfox.com/wkr4hi9v?key=57ee67e855faf38e4c7feb082dddb780',
+'https://arkgleamfox.com/wkr4hi9v?key=57ee67e855faf38e4c7feb082dddb780'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
