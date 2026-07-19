bb049e9708afa0dfdfd2b35386579c5d1294f866
idhoiphiy <idhoiphiy.o@gmail.com>
2026-07-19 09:26:54 +0700

    Create dadst2mingguan1.js

---
+++ dadst2mingguan1.js
@@ -0,0 +1,12 @@
+var dataUrl = [
+
+'https://hogencounter.com/mbgvn9zd9?key=1a3d45aa06d841e39c364a93afc2cf77',
+'https://hogencounter.com/mbgvn9zd9?key=1a3d45aa06d841e39c364a93afc2cf77',
+'https://hogencounter.com/vrkquizt?key=d205c14873f6d9f9e9433dea65aeab7a',
+'https://hogencounter.com/mbgvn9zd9?key=1a3d45aa06d841e39c364a93afc2cf77',
+'https://hogencounter.com/vrkquizt?key=d205c14873f6d9f9e9433dea65aeab7a',
+'https://hogencounter.com/vrkquizt?key=d205c14873f6d9f9e9433dea65aeab7a'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
