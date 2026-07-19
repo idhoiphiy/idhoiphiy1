3056d00aa3911871a8c77c0942ac712df91c3cef
idhoiphiy <idhoiphiy.o@gmail.com>
2026-07-19 09:24:33 +0700

    Create redadst2mingguan1.js

---
+++ redadst2mingguan1.js
@@ -0,0 +1,16 @@
+var dataUrl = [
+
+'https://nowwplayvideeeheree.blogspot.com/2026/07/play-video-10.html',
+'https://nowwplayvideeeheree.blogspot.com/2026/07/play-video-9.html',
+'https://nowwplayvideeeheree.blogspot.com/2026/07/play-video-8.html',
+'https://nowwplayvideeeheree.blogspot.com/2026/07/play-video-7.html',
+'https://nowwplayvideeeheree.blogspot.com/2026/07/play-video-6.html',
+'https://nowwplayvideeeheree.blogspot.com/2026/07/play-video-5.html',
+'https://nowwplayvideeeheree.blogspot.com/2026/07/play-video-4.html',
+'https://nowwplayvideeeheree.blogspot.com/2026/07/play-video-3.html',
+'https://nowwplayvideeeheree.blogspot.com/2026/07/play-video-2.html',
+'https://nowwplayvideeeheree.blogspot.com/2026/07/play-video-1.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
