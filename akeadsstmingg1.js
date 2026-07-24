34697a2bc58588ed9a20331e9e53b8b9d97d4f1b
idhoiphiy <idhoiphiy.o@gmail.com>
2026-07-24 07:46:07 +0700

    Create akeadsstmingg1.js

---
+++ akeadsstmingg1.js
@@ -0,0 +1,24 @@
+var dataUrl = [
+
+'https://impassableoffend.com/frik9vyd?key=13e375eb7e8fd8aa1f5cb283b4057627',
+'https://impassableoffend.com/frik9vyd?key=13e375eb7e8fd8aa1f5cb283b4057627',
+'https://impassableoffend.com/frik9vyd?key=13e375eb7e8fd8aa1f5cb283b4057627',
+'https://impassableoffend.com/frik9vyd?key=13e375eb7e8fd8aa1f5cb283b4057627',
+'https://impassableoffend.com/crya9k2x6?key=c3ddf144181d5fbd0dc73d00e1c280ac',
+'https://impassableoffend.com/crya9k2x6?key=c3ddf144181d5fbd0dc73d00e1c280ac',
+'https://impassableoffend.com/crya9k2x6?key=c3ddf144181d5fbd0dc73d00e1c280ac',
+'https://impassableoffend.com/crya9k2x6?key=c3ddf144181d5fbd0dc73d00e1c280ac',
+'https://impassableoffend.com/ij60gz3t?key=fd4ad9c418b3179535ae0bd09e43bdb4',
+'https://impassableoffend.com/ij60gz3t?key=fd4ad9c418b3179535ae0bd09e43bdb4',
+'https://impassableoffend.com/ij60gz3t?key=fd4ad9c418b3179535ae0bd09e43bdb4',
+'https://impassableoffend.com/ij60gz3t?key=fd4ad9c418b3179535ae0bd09e43bdb4',
+'https://impassableoffend.com/dk50270se?key=8c8632aee311b483dd1ad5a814003c94',
+'https://impassableoffend.com/dk50270se?key=8c8632aee311b483dd1ad5a814003c94',
+'https://impassableoffend.com/dk50270se?key=8c8632aee311b483dd1ad5a814003c94',
+'https://impassableoffend.com/wwam61979v?key=7c28ae78a5212632b867261f92ea162a',
+'https://impassableoffend.com/wwam61979v?key=7c28ae78a5212632b867261f92ea162a',
+'https://impassableoffend.com/wwam61979v?key=7c28ae78a5212632b867261f92ea162a'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
