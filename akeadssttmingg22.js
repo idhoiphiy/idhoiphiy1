4dcbe9cfd24ad5d40a3a50adbab109a092c19f4a
idhoiphiy <idhoiphiy.o@gmail.com>
2026-07-29 15:51:40 +0700

    Create akeadssttmingg22.js

---
+++ akeadssttmingg22.js
@@ -0,0 +1,12 @@
+var dataUrl = [
+
+'https://hogencounter.com/hk2155jtef?key=1a0c948a9c4ed927321fde17a592f713',
+'https://hogencounter.com/hk2155jtef?key=1a0c948a9c4ed927321fde17a592f713',
+'https://hogencounter.com/hk2155jtef?key=1a0c948a9c4ed927321fde17a592f713',
+'https://hogencounter.com/q05qzgcgu?key=fe46aae16e2592cd63f24f2fa640830b',
+'https://hogencounter.com/q05qzgcgu?key=fe46aae16e2592cd63f24f2fa640830b',
+'https://hogencounter.com/q05qzgcgu?key=fe46aae16e2592cd63f24f2fa640830b'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
