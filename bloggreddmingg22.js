99949a9fdc72c8b19e2e63777e50932ed11d41b9
idhoiphiy <idhoiphiy.o@gmail.com>
2026-07-29 16:15:34 +0700

    Create bloggreddmingg22.js

---
+++ bloggreddmingg22.js
@@ -0,0 +1,26 @@
+var dataUrl = [
+
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-20.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-19.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-18.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-17.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-16.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-15.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-14.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-13.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-12.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-11.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-10.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-9.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-8.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-7.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-6.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-5.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-4.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-3.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-2.html',
+'https://hddplayvideooonoww.blogspot.com/2026/07/play-video-1.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
