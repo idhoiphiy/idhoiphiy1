5c18ddcdd594c3391cf79db88c9c2ce252f36f5d
idhoiphiy <idhoiphiy.o@gmail.com>
2026-08-18 14:06:42 +0700

    Create akeucshaar1.js

---
+++ akeucshaar1.js
@@ -0,0 +1,12 @@
+var dataUrl = [
+
+'https://uc-share.com/s/09c0e0cb50624?la=ms',
+'https://uc-share.com/s/09c0e0cb50624?la=ms',
+'https://uc-share.com/s/09c0e0cb50624?la=ms',
+'https://uc-share.com/s/1aa6ed614a764?la=ms',
+'https://uc-share.com/s/1aa6ed614a764?la=ms',
+'https://uc-share.com/s/1aa6ed614a764?la=ms'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
