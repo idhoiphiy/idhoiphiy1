b25474dbbb3ad98adf69c8b5c26e417ec742ae76
idhoiphiy <idhoiphiy.o@gmail.com>
2026-08-19 10:06:01 +0700

    Create akeaadstmngg21.js

---
+++ akeaadstmngg21.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://hogencounter.com/zdvvcqkdq?key=eeb9370bb3f42f9f2da7068ac6680a2e',
+'https://hogencounter.com/zdvvcqkdq?key=eeb9370bb3f42f9f2da7068ac6680a2e',
+'https://hogencounter.com/zdvvcqkdq?key=eeb9370bb3f42f9f2da7068ac6680a2e'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
