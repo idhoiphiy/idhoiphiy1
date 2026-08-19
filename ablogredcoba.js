ce72ab9843c48a6ddad535cbd9095af16e780558
idhoiphiy <idhoiphiy.o@gmail.com>
2026-08-19 10:06:48 +0700

    Delete ablogredcoba.js

--- ablogredcoba.js
+++
@@ -1,8 +0,0 @@
-var dataUrl = [
-
-'https://filmasikin.blogspot.com/2026/06/paly-video-1.html',
-'https://filmasikin.blogspot.com/2026/06/paly-video-2.html'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
