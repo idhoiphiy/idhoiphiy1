3e104fccb2bb4ded1013f664956385dcfb178822
idhoiphiy <idhoiphiy.o@gmail.com>
2026-08-19 10:04:43 +0700

    Delete akeadsmingg222.js

--- akeadsmingg222.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-
-'https://hogencounter.com/n3ab9hppum?key=50e2ebcf42b89de7cbc9b155c8f72787',
-'https://hogencounter.com/n3ab9hppum?key=50e2ebcf42b89de7cbc9b155c8f72787',
-'https://hogencounter.com/n3ab9hppum?key=50e2ebcf42b89de7cbc9b155c8f72787'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
